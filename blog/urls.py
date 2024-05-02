30ebcc3802149616b59b9803f5f7e7d5590bc494
SolijonovAsadbek <solijonovasadbek25111999@gmail.com>
2024-05-02 17:39:55 +0500

    2-may 17:39

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,38 @@
 from django.urls import path, include
 from rest_framework import routers
-
+from django.urls import re_path
 from blog.views import UserViewSet, PostViewSet
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'posts', PostViewSet)
 
+# API DOCS
+
+# ERRORS:
+# pip install --upgrade setuptools
+# pip install --upgrade distribute
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Blog API",
+        default_version='v1',
+        description="Post Description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="iamsolijonovasadbek@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 # new
 urlpatterns = [
     path('', include(router.urls)),
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',  # new
     'rest_framework',  # new
+    'drf_yasg'
 
 ]
 
