3062f677bd6765afaf8886cc5780f8f828ade6eb
SolijonovAsadbek <solijonovasadbek25111999@gmail.com>
2024-05-06 20:13:23 +0500

    6--may 20:07

--- config/urls.py
+++ config/urls.py
@@ -14,11 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from config import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api/v1/', include('blog.urls'))  # new
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # new
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  # new
