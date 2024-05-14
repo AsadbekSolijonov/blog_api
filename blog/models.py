a009a75245fd5ce9924a5dd9db8c75adcfc852df
SolijonovAsadbek <solijonovasadbek25111999@gmail.com>
2024-05-14 17:16:05 +0500

    14-may 17:16

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
+    image = models.ImageField(upload_to='', blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
