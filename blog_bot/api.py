cf9bd5c8e4205c6ee5bd3837a75122b31cefe8e3
SolijonovAsadbek <solijonovasadbek25111999@gmail.com>
2024-05-16 17:33:00 +0500

    16-may 17:32

---
+++ blog_bot/api.py
@@ -0,0 +1,19 @@
+import requests
+import json
+from pprint import pprint as pp
+
+url = "https://blog-api-2h5h.onrender.com/api/v1/posts/"
+
+payload = ""
+headers = {
+    'Authorization': 'Basic YWRtaW46MTIzNA=='
+}
+
+
+def get_posts():
+    try:
+        response = requests.request("GET", url, headers=headers, data=payload)
+        my_json = json.loads(response.text)
+        return my_json
+    except Exception as e:
+        return "Nice Try!"
--- blog_bot/bot_run.py
+++ blog_bot/bot_run.py
@@ -8,51 +8,31 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
+from blog_bot.api import get_posts
 
 # Bot token can be obtained via https://t.me/BotFather
-TOKEN = "6669615336:AAErKl5qtANGu9PKRT4oo6ag5RIocAPskGM"
+TOKEN = "6669615336:AAHoZW2A0w93z4Kyxd8WwmwE32jPfXNfiBc"
 
-# All handlers should be attached to the Router (or Dispatcher)
 dp = Dispatcher()
 
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
-    """
-    This handler receives messages with `/start` command
-    """
-    # Most event objects have aliases for API methods that can be called in events' context
-    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
-    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
-    # method automatically or call API method directly via
-    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
     await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
 
 
 @dp.message(Command('posts'))
 async def posts_handler(message: Message) -> None:
-    pass
-
-
-@dp.message()
-async def echo_handler(message: Message) -> None:
-    """
-    Handler will forward receive a message back to the sender
-
-    By default, message handler will handle all message types (like a text, photo, sticker etc.)
-    """
     try:
-        # Send a copy of the received message
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        # But not all the types is supported to be copied so need to handle it
-        await message.answer("Nice try!")
+        datas = get_posts()
+        datas = datas if datas else "Ma'lumot qo'shing"
+        await message.answer(f"{datas}")
+    except ConnectionError as e:
+        await message.answer("Nice Try! ")
 
 
 async def main() -> None:
-    # Initialize Bot instance with default bot properties which will be passed to all API calls
     bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-    # And the run events dispatching
     await dp.start_polling(bot)
 
 
