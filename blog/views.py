7245e9a717499dd9b38a1b30ab4d26c6cc409447
SolijonovAsadbek <solijonovasadbek25111999@gmail.com>
2024-05-23 17:14:28 +0500

    23-may 17:14

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,6 @@ from blog.serializers import UserSerializer, PostSerializer
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 
 
-
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
