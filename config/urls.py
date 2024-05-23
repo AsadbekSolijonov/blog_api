76a5af3de4f7afd6fb0b6bec0ceb9408838821da
SolijonovAsadbek <solijonovasadbek25111999@gmail.com>
2024-05-23 17:44:35 +0500

    23-may 17:14

--- config/urls.py
+++ config/urls.py
@@ -26,19 +26,19 @@ from rest_framework.response import Response
 from config import settings
 
 
-class CustomAuthToken(ObtainAuthToken):
-
-    def post(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data,
-                                           context={'request': request})
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data['user']
-        token, created = Token.objects.get_or_create(user=user)
-        return Response({
-            'token': token.key,
-            'user_id': user.pk,
-            'email': user.email
-        })
+# class CustomAuthToken(ObtainAuthToken):
+#
+#     def post(self, request, *args, **kwargs):
+#         serializer = self.serializer_class(data=request.data,
+#                                            context={'request': request})
+#         serializer.is_valid(raise_exception=True)
+#         user = serializer.validated_data['user']
+#         token, created = Token.objects.get_or_create(user=user)
+#         return Response({
+#             'token': token.key,
+#             'user_id': user.pk,
+#             'email': user.email
+#         })
 
 
 urlpatterns = [
